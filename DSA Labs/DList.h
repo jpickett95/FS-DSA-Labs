894343d0c93d7f9877e59c8e61a03b2cdf5ec43e
Jonah Pickett <86933257+jpickett95@users.noreply.github.com>
2022-05-12 22:31:46 -0400

    completed & PASSED NODE_CTOR();

--- DSA Labs/DList.h
+++ DSA Labs/DList.h
@@ -39,7 +39,7 @@ NOTE: If the unit test is not on, that code will not be compiled!
 // Individual unit test toggles
 #define LAB3_CTOR						1
 #define LAB3_NODE_CTOR_DEFAULT			1
-#define LAB3_NODE_CTOR					0
+#define LAB3_NODE_CTOR					1
 #define LAB3_ADDHEAD_EMPTY				0
 #define LAB3_ADDHEAD					0
 #define LAB3_ADDTAIL_EMPTY				0
